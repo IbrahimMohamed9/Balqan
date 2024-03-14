a230926cf4c1a62158fd4df4b5413c8931fb8fed
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-03-14 11:05:58 +0100

    remove reload from file

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const barIcon = document.querySelector(".dash .main-header .bar .bar-icon");
   const body = document.body;
   const hideMobileSpans = document.querySelectorAll(
-    ".page .sidebar ul li a i + span.hide-mobile"
+    ".page .sidebar ul li span i + span.hide-mobile"
   );
 
   barIcon.addEventListener("click", function () {
@@ -21,10 +21,75 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  // Change .page .sidebar.p-20 to .page .sidebar.p-10
   const sidebarElements = document.querySelectorAll(".page .sidebar.p-20");
   sidebarElements.forEach((element) => {
     element.classList.remove("p-20");
     element.classList.add("p-10");
   });
 });
+
+const profilePage = document.querySelector(".content .profile-page");
+const projectsPage = document.querySelector(".content .projects-page");
+const settingsPage = document.querySelector(".content .settings-page");
+const friendsPage = document.querySelector(".content .friends-page");
+const filesPage = document.querySelector(".content .files-page");
+const dashboardPage = document.querySelector(".content .wrapper");
+
+const profileIcon = document.getElementById("profile");
+const settingsIcon = document.getElementById("settings");
+const projectsIcon = document.getElementById("projects");
+const friendsIcon = document.getElementById("friends");
+const filesIcon = document.getElementById("files");
+const dashboardIcon = document.getElementById("dashboard");
+
+function switchPage(clickedIcon, targetPage) {
+  targetPage.classList.remove("d-none");
+  clickedIcon.classList.add("active");
+
+  const pages = [
+    profilePage,
+    projectsPage,
+    settingsPage,
+    friendsPage,
+    filesPage,
+    dashboardPage,
+  ];
+
+  const icons = [
+    profileIcon,
+    settingsIcon,
+    projectsIcon,
+    friendsIcon,
+    filesIcon,
+    dashboardIcon,
+  ];
+
+  for (const page of pages) {
+    if (page !== targetPage) {
+      page.classList.add("d-none");
+    }
+  }
+
+  for (const icon of icons) {
+    if (icon !== clickedIcon) {
+      icon.classList.remove("active");
+    }
+  }
+}
+
+profileIcon.addEventListener("click", () =>
+  switchPage(profileIcon, profilePage)
+);
+settingsIcon.addEventListener("click", () =>
+  switchPage(settingsIcon, settingsPage)
+);
+projectsIcon.addEventListener("click", () =>
+  switchPage(projectsIcon, projectsPage)
+);
+friendsIcon.addEventListener("click", () =>
+  switchPage(friendsIcon, friendsPage)
+);
+filesIcon.addEventListener("click", () => switchPage(filesIcon, filesPage));
+dashboardIcon.addEventListener("click", () =>
+  switchPage(dashboardIcon, dashboardPage)
+);
