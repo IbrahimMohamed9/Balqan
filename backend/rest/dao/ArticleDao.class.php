002dfa7e11b4b8d9cc03237bac56026d39205fe8
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-04-16 02:02:21 +0200

    article

---
+++ backend/rest/dao/ArticleDao.class.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once __DIR__ . '/BaseDao.class.php';
+
+class ArticleDao extends BaseDao {
+    public function __construct() {
+        parent::__construct('articles');
+    }
+
+    public function add_article($article) {
+        $table = $this->getTable();
+
+        return $this->insert($table, $article);
+    }
+
+    public function get_articles() {
+        $query = "SELECT * FROM articles";
+        return $this->query($query);
+    }
+
+    public function get_article_by_id($article_id) {
+        $query = "SELECT * FROM articles WHERE article_id = :article_id";
+        return $this->query_unique($query, ['article_id' => $article_id]);
+    }
+
+    public function delete_article($article_id) {
+        $query = "DELETE FROM articles WHERE article_id = :article_id";
+        return $this->execute($query, ['article_id' => $article_id]);
+    }
+    
+    public function edit_article($article_id, $article) {
+        
+        $query = "UPDATE items SET 
+            article_id = :article_id, 
+            img = :img, 
+            imgDesc = :imgDesc, 
+            stock_quantity = :stock_quantity, 
+            imgs_srcs = :imgs_srcs, 
+            category = :category, 
+            title = :title, 
+            country = :country, 
+            time = :time, 
+            description = :description, 
+            content = :content";
+    
+        $this->execute($query, $article);
+    }
+}
---
+++ backend/rest/services/articleService.class.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/../dao/ArticleDao.class.php';
+
+class articleService {
+    private $article_dao;
+    public function __construct() {
+        $this->article_dao = new ArticleDao();
+    }
+    public function add_article($article){
+        return $this->article_dao->add_article($article);
+    }
+    public function get_articles(){
+        return $this->article_dao->get_articles();
+    }
+    public function get_article_by_id($article_id) {
+        return $this->article_dao->get_article_by_id($article_id);
+    }
+    public function delete_article($article_id) {
+        return $this->article_dao->delete_article($article_id);
+    }
+    public function edit_article($article) {
+        $article_id = $article['article_id'];
+        unset($article['article_id']);
+
+        $this->article_dao->edit_article($article_id, $article);
+    }
+}
