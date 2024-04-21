77d2343a6fa5535a6f385d7540b48347811499f8
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-04-21 02:34:19 +0200

    update

--- backend/feedbacks/add_feedback.php
+++ backend/feedbacks/add_feedback.php
@@ -1,10 +1,17 @@
 <?php
-require_once __DIR__ . '/../rest/services/CartService.class.php';
+require_once __DIR__ . '/../rest/services/FeedbackService.class.php';
 
 $payload = $_REQUEST;
 
-$cart_service = new CartService();
+$feedback_service = new FeedbackService();
 
-$cart = $cart_service->add_item_cart($payload);
-
-echo json_encode(['message'=> "you have successfully added the cart", 'data'=> $cart]);
+if($payload['feedback_id'] != NULL && $payload['feedback_id'] != ''){
+  $feedback = $feedback_service->edit_feedback($payload);
+} else {
+  unset($payload['feedback_id']);
+  if(empty($payload['added_time'])){
+      unset($payload['added_time']);
+  }
+  $feedback = $feedback_service->add_feedback($payload);
+}
+echo json_encode(['message'=> "you have successfully added the feedback", 'data'=> $feedback]);
