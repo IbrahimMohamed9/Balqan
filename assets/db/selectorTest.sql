b548a87530cedd240e69f070a744be2b8fe9eb57
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-05-07 03:02:07 +0200

    finish part of users document

--- assets/db/selectorTest.sql
+++ assets/db/selectorTest.sql
@@ -8,7 +8,11 @@ SELECT *
 FROM items;
 
 SELECT *
+FROM targets;
+
+SELECT user_id, password, phone, name
 FROM balqan.users;
+
 SELECT * FROM articles WHERE article_id = :article_id;
 SELECT *
 FROM projects;
@@ -30,4 +34,4 @@ WHERE category = 'package'
 ORDER BY added_time
 LIMIT 2;
 
-SELECT * FROM items;
+SELECT * FROM activities;
